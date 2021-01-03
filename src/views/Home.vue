185a6752132f5c2ff6bd742198881c0d624dbdf3
mikerosillo <angelrosillo2018Qgmail.com>
2021-01-03 01:26:37 -0600

    project imported

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <!--<img alt="Vue logo" src="../assets/logo.png">-->
+    <HelloWorld msg="passing props this is a prop of a component"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
